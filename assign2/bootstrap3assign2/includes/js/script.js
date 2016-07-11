6f31802f5636327d27dc8caaadd039c36370f9e4
komalimallidi <velpurisaranya@gmail.com>
2016-07-11 11:56:02 +0530

    bootstrap

---
+++ assign2/bootstrap3assign2/includes/js/script.js
@@ -0,0 +1,18 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$(function(){	
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+$("[rel='tooltip']").tooltip();
+});
+
